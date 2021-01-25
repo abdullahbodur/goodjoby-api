904bc9a93b0a4eb4e58b8cd21f133cae34bdc6c0
abdullahbodur <abdullahbodur.abbdr@gmail.com>
2021-01-25 12:35:43 +0300

    fixed # CORS Protocol Errors for Frontend acess  ✔⚙

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const connectDatabase = require("./helpers/database/databaseHelper");
 const apiRouter = require("./router/index");
 const customErrorHandler = require("./middlewares/errors/customErrorHandling");
 const path = require("path");
+const accessControlMiddleware = require("./middlewares/protocolMiddlewares.js/protocolMiddleware");
 
 dotenv.config({
   path: "./config/env/config.env",
@@ -12,16 +13,20 @@ dotenv.config({
 connectDatabase();
 
 const app = express();
-// mrb
+
+app.disable("x-powered-by");
 app.use(express.json());
 
+app.use(accessControlMiddleware);
+
 app.use("/api", apiRouter);
+
 app.use(customErrorHandler);
 
 app.use(express.static(path.join(__dirname + "/public")));
 
 const PORT = process.env.PORT;
-  
+
 app.listen(PORT, () => {
   console.log(`Server Started at ${PORT}`);
 });
